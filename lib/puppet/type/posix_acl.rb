6789c7c8fa817a4b21a72b651be7970a856347b1
Adam Vessey <adam-vessey@users.noreply.github.com>
2018-03-16 12:54:13 -0300

    Support uppercase X.
    Traversable directories, with files only executable if they're executable
    by _someone_.

--- lib/puppet/type/posix_acl.rb
+++ lib/puppet/type/posix_acl.rb
@@ -215,7 +215,8 @@ Puppet::Type.newtype(:posix_acl) do
         s = p.tr '-', ''
         r << (s.sub!('r', '') ? 'r' : '-')
         r << (s.sub!('w', '') ? 'w' : '-')
-        r << (s.sub!('x', '') ? 'x' : '-')
+        x = s.match(/^x$/i)
+        r << (s.sub!(/^x$/i, '') ? x : '-')
         raise ArgumentError, %(Invalid permission set "#{p}".) unless s.empty?
       end
       r
